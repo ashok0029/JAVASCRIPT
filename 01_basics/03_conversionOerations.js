b79c677580ef59369619165646d09df48cfdeaa8
Ashok Bishnoi <111235303+ashok0029@users.noreply.github.com>
2024-09-18 12:49:22 +0000

    operators

--- 01_basics/03_conversionOerations.js
+++ 01_basics/03_conversionOerations.js
@@ -27,3 +27,46 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ********** Operations ***********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Ashok"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2 => ok but not the right way to write
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// To study more about the prefix and postfix operators use below link
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
