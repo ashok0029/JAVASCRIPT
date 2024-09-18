26396dc1d591370d831a54128f9bc3a9f64720ba
Ashok Bishnoi <111235303+ashok0029@users.noreply.github.com>
2024-09-18 12:22:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOerations.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1; false=> 0;
+// "Ashok" => NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+// 1 => true; 0=> false;
+// "" => false
+// "Ashok" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
