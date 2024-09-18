aa5e38b71b1f7ffc129ed8ff63f6b048d5ce42b8
Ashok Bishnoi <111235303+ashok0029@users.noreply.github.com>
2024-09-18 15:09:50 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,48 @@
+// const string = "Ashok Bishnoi"
+// console.log(string);
+
+// normal method 
+// const name = "Ashok Bishnoi"
+// const age = 20
+// console.log(name+age);
+
+
+
+// String interpolation --> ` `
+// Another option of string concatenation, is string interpolation, which substitutes values of variables into placeholders in a string. Note that you do not have to worry about spaces, like with concatenation
+// String interpolation provides a more readable, convenient syntax to format strings.
+
+// const name = "Ashok Bishnoi"
+// const age = 20
+// console.log(`my name is ${name} and my  age is ${age} years`);
+
+
+
+// another way to declare a string is 
+
+// const gameName = new String("ashokbishnoi")
+// // different methods/fuctions of string
+// console.log(gameName.length);
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('b'));
+
+// more methods like substring, slice and many more you can read throw documentation.
+
+const newGame = new String('ashok-bishn-oi')
+console.log(newGame);
+const newString = newGame.substring(0,5)
+console.log(newString);
+const anotherString = newGame.slice(-13,7)
+console.log(anotherString);
+const anotherNewString = newGame.split('-',3)
+console.log(anotherNewString);
+
+
+
+
+
+
+
