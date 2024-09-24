77ee88af2fca4ce289849a723bdede8d22a6ce3f
Ashok Bishnoi <111235303+ashok0029@users.noreply.github.com>
2024-09-24 12:34:17 +0000

    api-json basic done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -4,7 +4,7 @@ appUser.id = "123abc"
 appUser.name = "ashok"
 appUser.isLoggedIn = false
 
-// console.log(appUser);
+console.log(appUser);
 
 const regularUser = {
     email: "some@google.com",
@@ -15,7 +15,7 @@ const regularUser = {
         }
     }
 }
-// console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname);
 
 // combining objects
 
@@ -27,24 +27,24 @@ const obj4 = {obj1,obj2} // normal method
 
 // using assign 
 
-// const obj5 = Object.assign(obj1, obj2, obj3) // if we doen't apply {} .all the values goes in obj1 as well. for example-->
-// console.log(obj5);
-// console.log(obj1);
+const obj5 = Object.assign(obj1, obj2, obj3) // if we doen't apply {} .all the values goes in obj1 as well. for example-->
+console.log(obj5);
+console.log(obj1);
 // expected output
 // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 
 //  But if the we apply then onj1 doesn't changes
-const obj5 = Object.assign({},obj1, obj2, obj3)
-// console.log(obj5);
-// console.log(obj1);
+const obj9 = Object.assign({},obj1, obj2, obj3)
+console.log(obj9);
+console.log(obj1);
 // Expected Output 
 // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
 // { '1': 'a', '2': 'b' }
 
 // using spread operator
 const obj6 ={...obj1, ...obj2}
-// console.log(obj6);
+console.log(obj6);
 
 // let suppose values are coming from databases in the form of array.
 // array of objects.
@@ -73,4 +73,30 @@ console.log(appUser.hasOwnProperty('isLoggedIn'));
 
 
 
+// object destructuring
+const course = {
+    coursename: "Javascript",
+    price: "9999",
+    courseInstructor: "ashok"
+}
 
+const {courseInstructor: instructor} = course
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+// api
+// json format
+// {
+//     "name": "ashok",
+//     "coursename": "Javasript",
+//     "price": "free"
+
+// }
+
+// api in the form of array of objects 
+// [
+//     {},
+//     {},
+//     {}
+// ]
